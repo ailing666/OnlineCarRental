daa70a9aaae9097000564daa283710c8f7eced61
TENCENT\v_llingai <1120877019@qq.com>
2021-01-27 14:28:23 +0800

    feat:单个cars-item完成

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="cars">
+    <cars-list></cars-list>
     <span @click="toUser">cars</span>
   </div>
 </template>
 
 <script>
+import carsList from '@/components/carsList/index.vue'
 export default {
   name: 'cars',
+  components: { carsList },
   methods: {
     // 路由跳转至 “我的”
     toUser(){
